b48c8998b415229c5b8cded90c7d659e2f34f3af
patelshaunak5098 <patelshaunak5098@gmail.com>
2024-07-04 11:40:21 -0400

    updated lambda function added sagemaker endpoint

--- scripts/lambda_function.py
+++ scripts/lambda_function.py
@@ -1,6 +1,38 @@
+import boto3
+import json
+
+
 def handler(event, context):
 
     # Your logic goes here
+
+    # Initialize the SageMaker runtime client
+    runtime = boto3.client('sagemaker-runtime', region_name='us-east-1')
+
+    # Your endpoint name
+    endpoint_name = 'xgboost-2024-07-04-15-28-03-321'
+
+    data = [13.88,5.4,2.23,19,85,0.98,0.34,0.4,0.68,4.9,0.50,1.30,420]
+    print(data)
+    payload = ','.join([str(item) for item in data])
+    print(payload)
+    # Invoke the endpoint
+
+    response = runtime.invoke_endpoint(EndpointName=endpoint_name,
+                                    ContentType='text/csv',
+                                    Body=payload)
+    print(response)
+
+    # Parse response
+    result = json.loads(response['Body'].read().decode())
+    print(result)
+
+    return {
+        'statusCode': 200,
+        'body': response
+    }
+
+    '''
     message = "Here you Go!!!"
     print('Hello, world from GET Lambda Func! This is from Terraform')
     print('updated!!!!!!!!!!')
@@ -8,3 +40,5 @@ def handler(event, context):
         'statusCode': 200,
         'body': message
     }
+    '''
+
