4bceb827838647a29a7c31ad1e2b2be0d9b6b51d
patelshaunak5098 <patelshaunak5098@gmail.com>
2024-07-04 10:59:54 -0400

    updated lambda function

--- scripts/lambda_function.py
+++ scripts/lambda_function.py
@@ -1,7 +1,7 @@
 def handler(event, context):
 
     # Your logic goes here
-    message = "Hello, world from GET Lambda Func! This is Sagemaker"
+    message = "Hello, world from GET Lambda Func! This is Sagemaker!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
     print('Hello, world from GET Lambda Func! This is from Terraform')
     print('updated!!!!!!!!!!')
     return {
