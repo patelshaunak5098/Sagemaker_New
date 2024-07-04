4a13a44e7106f7e985ad7a1e358394c10e7a0cfc
patelshaunak5098 <patelshaunak5098@gmail.com>
2024-07-04 12:29:25 -0400

    updated lambda function for data input 8

--- scripts/lambda_function.py
+++ scripts/lambda_function.py
@@ -15,9 +15,15 @@ def handler(event, context):
     endpoint_name = 'xgboost-2024-07-04-15-28-03-321'
 
     data = [13.88,5.4,2.23,19,85,0.98,0.34,0.4,0.68,4.9,0.50,1.30,420]
-    dummy = event['body']['data']
+    
+    body = json.loads(event['body'])
+    dummy = body['data']
 
-    print("dummy ===== ",dummy.data)
+    dummy2 = body.data
+
+    print("dummy ===== ",dummy)
+    print("dummy2 ===== ",dummy2)
+
     
     print(data)
     payload = ','.join([str(item) for item in data])
